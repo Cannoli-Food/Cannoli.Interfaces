5ac2f180e5158f42ba97cd32cfb383d86c072202
MukaDeveloper <muka.bertoni@gmail.com>
2024-11-08 15:31:15 -0300

    v1.5.41

--- src/mobyo/mobyo-api/interfaces/i-order-customer-v3.ts
+++ src/mobyo/mobyo-api/interfaces/i-order-customer-v3.ts
@@ -1,43 +1,39 @@
-
 // #region interfacees (2)
 
-import { ICoords } from "../../../general";
-import { IUsedVoucher } from "../../mobyo-customer-mobyo";
+import { ICoords } from '../../../general';
 
 export interface IOrdersCustomerPhone {
-    // #region Properties (4)
+  // #region Properties (4)
 
-    
-    description: String;
-    
-    localizer: String;
-    
-    localizerExpiration: String;
-    
-    number: String;
+  description: string;
 
-    // #endregion Properties (4)
+  localizer: string;
+
+  localizerExpiration: string;
+
+  number: string;
+
+  // #endregion Properties (4)
 }
 
 export interface IOrderCustomerV3 {
-    // #region Properties (7)
+  // #region Properties (7)
 
-    
-    documentNumber: string;
-    
-    email: string;
-    
-    id: string;
-    
-    name: string;
-    
-    ordersCountOnMerchant: number;
-    
-    phone: IOrdersCustomerPhone;
-    
-    picture: string;
-    birthday: Date | null;
-    coords: ICoords | null;
+  documentNumber: string;
+
+  email: string;
+
+  id: string;
+
+  name: string;
+
+  ordersCountOnMerchant: number;
+
+  phone: IOrdersCustomerPhone;
+
+  picture: string;
+  birthday: Date | null;
+  coords: ICoords | null;
 }
 
 // #endregion interfacees (2)
