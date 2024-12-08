d9f6d182c53f339ffe889fc4a5a314d56815cb1b
Mark Cecon <mark.cecon@gmail.com>
2024-12-07 22:18:50 -0300

    feat: implement deviceId property in PayioOrderEntity and IPayioOrder interface; bump version to 1.6.35 in package.json

--- src/payio/orders/entities/order.entity.ts
+++ src/payio/orders/entities/order.entity.ts
@@ -1,9 +1,16 @@
 import { OrderEntity } from '../../../order';
+import { IPayioOrder } from '../interfaces';
 import { PayioOrderIndoorEntity } from './indoor.entity';
 
-export class PayioOrderEntity extends OrderEntity {
-  // #region Constructors (1)
+export class PayioOrderEntity extends OrderEntity implements IPayioOrder {
+  // #region Properties (2)
+
+  public deviceId: string | null = null;
   public indoor: PayioOrderIndoorEntity | null = null;
+
+  // #endregion Properties (2)
+
+  // #region Constructors (1)
 
   constructor(data?: Partial<PayioOrderEntity>) {
     super();
--- src/payio/orders/interfaces/i-order.ts
+++ src/payio/orders/interfaces/i-order.ts
@@ -4,6 +4,7 @@ import { IPayioOrderIndoor } from './i-order-indoor';
 export interface IPayioOrder extends IOrder {
   // #region Properties (10)
   indoor: IPayioOrderIndoor | null;
+  deviceId: string | null;
 
   // #endregion Properties (10)
 }
