1ac254392a86a2021710b32b22726248eba6ddcd
MukaDeveloper <muka.bertoni@gmail.com>
2025-04-01 14:27:36 -0300

    v1.0.9 handshake interfaces & enums

--- src/ifood/interfaces/i-ifood-handshake-response.ts
+++ src/ifood/interfaces/i-ifood-handshake-response.ts
@@ -1,9 +1,10 @@
+import { EIFoodHandshakeStatus } from "../enums";
 import { IIFoodHandshakeSelectedAlternatives } from "./i-ifood-handshake-selected-alternatives";
 
 export interface IIFoodHandshake {
 	id: string;
 	disputeId: string;
-	status: string;
+	status: EIFoodHandshakeStatus;
 	reason?: string;
 	selectedDisputeAlternative?: IIFoodHandshakeSelectedAlternatives;
 }
