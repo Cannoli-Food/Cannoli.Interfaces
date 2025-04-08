c6d649109fc2494be755b085ad3b43c891bdffed
MukaDeveloper <muka.bertoni@gmail.com>
2025-04-08 12:23:54 -0300

    Alterando key-type.enum, CPF & CNPJ alterado para DOCUMENT e inserindo SOCIAL_NUMBER v1.0.15

--- src/company/enums/key-type.enum.ts
+++ src/company/enums/key-type.enum.ts
@@ -1,7 +1,23 @@
-export enum EPixKeyTypes{
-    PHONE = 'PHONE',
-    EMAIL = 'EMAIL',
-    CPF = 'CPF',
-    CNPJ = 'CNPJ',
-    RANDOM_KEY = 'RANDOM_KEY'
+export enum EPixKeyTypes {
+  /**
+   * Telefone
+   */
+  PHONE = 'PHONE',
+  /**
+   * Email
+   */
+  EMAIL = 'EMAIL',
+  /**
+   * Documento CPF ou CNPJ
+   */
+  DOCUMENT = 'DOCUMENT',
+  /**
+   * Chave aleatóoria
+   */
+  RANDOM_KEY = 'RANDOM_KEY',
+  /**
+   * Usado em alguns contextos internacionais,
+   * como documento de identidade estrangeiro, similar ao CPF
+   */
+  SOCIAL_NUMBER = 'SOCIAL_NUMBER',
 }
