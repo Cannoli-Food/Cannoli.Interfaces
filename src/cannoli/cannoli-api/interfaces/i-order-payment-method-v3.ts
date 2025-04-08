985cf66a7bbbd0f0cb99d3eced072c7ef929f5ff
MukaDeveloper <muka.bertoni@gmail.com>
2025-04-08 09:36:37 -0300

    pixQrCode in IOrderPaymentPix v1.0.14

--- src/cannoli/cannoli-api/interfaces/i-order-payment-method-v3.ts
+++ src/cannoli/cannoli-api/interfaces/i-order-payment-method-v3.ts
@@ -1,27 +1,15 @@
-import { EOrderPaymentMethod } from '../enum/order-payment-method.enum';
-
-// #region Interfaces (5)
+import { EOrderPaymentMethod } from "../enum/order-payment-method.enum";
 
 export interface IOrderPaymentCard {
-  // #region Properties (2)
-
   brand: string | null;
   link: string | null;
-
-  // #endregion Properties (2)
 }
 
 export interface IOrderPaymentCash {
-  // #region Properties (1)
-
   changeFor: Number;
-
-  // #endregion Properties (1)
 }
 
 export interface IOrderPaymentMethodV3 {
-  // #region Properties (14)
-
   card: IOrderPaymentCard;
   cash: IOrderPaymentCash;
   code: string;
@@ -36,25 +24,14 @@ export interface IOrderPaymentMethodV3 {
   type: 'ONLINE' | 'OFFLINE';
   value: number;
   wallet: IOrderPaymentWallet;
-
-  // #endregion Properties (14)
 }
 
 export interface IOrderPaymentPix {
-  // #region Properties (2)
-
   pixKey: string;
   urlQrImage: string;
-
-  // #endregion Properties (2)
+  pixQrCode: string,
 }
 
 export interface IOrderPaymentWallet {
-  // #region Properties (1)
-
   name: string;
-
-  // #endregion Properties (1)
 }
-
-// #endregion Interfaces (5)
